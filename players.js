2cd7886c7654d96de76227feb9f967c45b716c17
achref332 <achrefhayouni2021@gmail.com>
2025-09-20 17:18:23 +0100

    Create players.js

---
+++ players.js
@@ -0,0 +1,37 @@
+// src/players.js
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentine",
+    jerseyNumber: 30,
+    age: 36,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/89/Messi_2018.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 38,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 25,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/87/Kylian_Mbappe_2022.jpg"
+  },
+  {
+    name: "Neymar Jr",
+    team: "Paris Saint-Germain",
+    nationality: "Brésil",
+    jerseyNumber: 10,
+    age: 31,
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/09/Neymar_2020.jpg"
+  }
+];
+
+export default players;
